c89de74be7e6c76d379402f761f29b2433c1cfa9
Artem Lukoianov <lukartoil@gmail.com>
2020-01-26 05:09:02 +0100

    upd

--- custom_utils.py
+++ custom_utils.py
@@ -5,6 +5,7 @@ import time
 import skimage.measure
 import subprocess
 import random
+import pandas as pd
 from tqdm import tqdm_notebook
 import matplotlib.pyplot as plt
 
@@ -100,7 +101,7 @@ def create_mesh(
     sdf_values = sdf_values.reshape(N, N, N)
 
     end = time.time()
-    print("sampling takes: %f" % (end - start))
+    #print("sampling takes: %f" % (end - start))
 
     return convert_sdf_samples_to_ply(
         sdf_values.data.cpu(),
@@ -111,6 +112,29 @@ def create_mesh(
         scale,
     )
 
+
+def get_latent_from_mesh(decoder, latent_size):
+    process_mesh('../Expirements/data/original_mesh.ply', '../Expirements/data/original_SDF.npz', 
+             'bin/PreprocessMesh', [])
+    
+    data_sdf = deep_sdf.data.read_sdf_samples_into_ram('../Expirements/data/original_SDF.npz')
+#     data_sdf[0] = data_sdf[0][torch.randperm(data_sdf[0].shape[0])]
+#     data_sdf[1] = data_sdf[1][torch.randperm(data_sdf[1].shape[0])]
+    
+    err, latent = reconstruct(
+                decoder,
+                500,
+                latent_size,
+                data_sdf,
+                0.01,  # [emp_mean,emp_var],
+                0.1,
+                num_samples=50,
+                lr=5e-3,
+                l2reg=True,
+            )
+    return latent
+    
+    
 
 def convert_sdf_samples_to_ply(
     pytorch_3d_sdf_tensor,
@@ -203,3 +227,151 @@ def plot_mesh_from_vector(decoder, initial_latent, N=256):
     
     cloud = PyntCloud.from_file('../Expirements/data/original_mesh.ply')
     cloud.plot(background='white', initial_point_size=0.003)
+
+def get_cloud_from_latent(decoder, initial_latent, N=256, save_path=None):
+    ply_mesh = None
+
+    with torch.no_grad():
+        ply_mesh = create_mesh( decoder,
+                                initial_latent,
+                                N=N,
+                                max_batch=int(2 ** 18),
+                                offset=None,
+                                scale=None)
+        
+    if save_path is None:
+        save_path = '../Expirements/data/original_mesh.ply'
+
+    ply_mesh.write(save_path)
+    
+    cloud = PyntCloud.from_file(save_path)
+    return cloud
+
+def subsample_from_torch_tensor(tensor, num_points):
+    if num_points < tensor.size(0):
+        return tensor[torch.randperm(tensor.size(0))[:num_points]]
+    else:
+        return tensor
+
+def get_points_from_latent(decoder, initial_latent, N=256, point_num=None, save_path=None):
+    cloud = get_cloud_from_latent(decoder, initial_latent, N, save_path=save_path)
+    nparr = cloud.points.to_numpy()
+    if point_num is not None:
+        nparr = random.sample(list(nparr), point_num)
+    return torch.cuda.FloatTensor(nparr)
+
+
+def plot_points_from_torch(tensor):
+    cloud = PyntCloud(pd.DataFrame(tensor.detach().cpu().numpy(), columns=['x', 'y', 'z']))
+    cloud.plot(background='white', initial_point_size=0.003)
+    
+def chamfer_distance_without_batch(p1, p2, debug=False):
+
+    '''
+    Calculate Chamfer Distance between two point sets
+    :param p1: size[1, N, D]
+    :param p2: size[1, M, D]
+    :param debug: whether need to output debug info
+    :return: sum of Chamfer Distance of two point sets
+    '''
+
+    assert p1.size(0) == 1 and p2.size(0) == 1
+    assert p1.size(2) == p2.size(2)
+
+    if debug:
+        print(p1[0][0])
+
+    p1 = p1.repeat(p2.size(1), 1, 1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+
+    p1 = p1.transpose(0, 1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+        print(p1[0])
+
+    p2 = p2.repeat(p1.size(0), 1, 1)
+    if debug:
+        print('p2 size is {}'.format(p2.size()))
+        print(p2[0])
+
+    dist = torch.add(p1, torch.neg(p2))
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist[0])
+
+    dist = torch.norm(dist, 2, dim=2)
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.min(dist, dim=1)[0]
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.sum(dist)
+    if debug:
+        print('-------')
+        print(dist)
+
+    return dist
+
+
+def chamfer_distance_with_batch(p1, p2, debug):
+
+    '''
+    Calculate Chamfer Distance between two point sets
+    :param p1: size[B, N, D]
+    :param p2: size[B, M, D]
+    :param debug: whether need to output debug info
+    :return: sum of all batches of Chamfer Distance of two point sets
+    '''
+
+    assert p1.size(0) == p2.size(0) and p1.size(2) == p2.size(2)
+
+    if debug:
+        print(p1[0])
+
+    p1 = p1.unsqueeze(1)
+    p2 = p2.unsqueeze(1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+        print('p2 size is {}'.format(p2.size()))
+        print(p1[0][0])
+
+    p1 = p1.repeat(1, p2.size(2), 1, 1)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+
+    p1 = p1.transpose(1, 2)
+    if debug:
+        print('p1 size is {}'.format(p1.size()))
+        print(p1[0][0])
+
+    p2 = p2.repeat(1, p1.size(1), 1, 1)
+    if debug:
+        print('p2 size is {}'.format(p2.size()))
+        print(p2[0][0])
+
+    dist = torch.add(p1, torch.neg(p2))
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist[0])
+
+    dist = torch.norm(dist, 2, dim=3)
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.min(dist, dim=2)[0]
+    if debug:
+        print('dist size is {}'.format(dist.size()))
+        print(dist)
+
+    dist = torch.sum(dist)
+    if debug:
+        print('-------')
+        print(dist)
+
+    return dist
